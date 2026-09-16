21887defd5636839fceba99ef6760aacdc1031d4
freddie999 <frederick.abalo@icloud.com>
2026-09-16 12:22:57 -0400

    Complete JavaScript exercises

--- exercises/part1-fizzbuzz/index.js
+++ exercises/part1-fizzbuzz/index.js
@@ -25,7 +25,17 @@ Start code
 
 ===================== */
 
-
+for (let number = 1; number <= 100; number += 1) {
+  if (number % 15 === 0) {
+    console.log('FizzBuzz');
+  } else if (number % 3 === 0) {
+    console.log('Fizz');
+  } else if (number % 5 === 0) {
+    console.log('Buzz');
+  } else {
+    console.log(number);
+  }
+}
 
 
 /* =====================
